bf58cb3f9baa4ca16fbd650df6ac79ff9032fceb
bielpeclat <bielpeclat1@gmail.com>
2026-06-24 18:55:56 +0100

    inicio dos exercicios

---
+++ template.asm
@@ -0,0 +1,29 @@
+.8086
+.model  small
+.stack  2048
+
+dseg    segment para public 'data'
+    ; --- DEFINICAO DE VARIAVEIS ---
+    
+dseg    ends
+
+cseg    segment para public 'code'
+    assume  cs:cseg, ds:dseg
+
+main    proc
+    ; Inicializacao do segmento de dados
+    mov     ax, dseg
+    mov     ds, ax
+
+    ; --- O TEU CODIGO COMECA AQUI ---
+
+
+    ; --- O TEU CODIGO TERMINA AQUI ---
+
+fim:
+    mov     ah, 4ch    ; Funcao para terminar o programa
+    int     21h        ; Interrupcao do DOS
+main    endp
+
+cseg    ends
+end     main
