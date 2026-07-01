b705000ce1ddfa651d0eead67a5b797ee0ada4b9
bielpeclat <bielpeclat1@gmail.com>
2026-07-01 09:34:16 +0100

    dia 1 julho

---
+++ ex_exames/22_23_normal_ex_2.asm
@@ -0,0 +1,16 @@
+MostrasLetras proc
+
+    ; string termina com 0
+    ; bx aponta para a string
+    ; al -> linha           ah -> coluna
+    ; dl -> direcao para escrever
+
+    ; vamos calcular o offset
+    ; offset = (linha * 160) + (coluna * 2)
+    
+
+    
+    
+
+    ret
+MostrasLetras endp
