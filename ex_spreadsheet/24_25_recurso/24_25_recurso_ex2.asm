bbc300df959cfadcae977d49b98f9fadcf112389
bielpeclat <bielpeclat1@gmail.com>
2026-07-06 15:44:53 +0100

    fazendo o 24/25 ex. 2 pratico

--- ex_spreadsheet/24_25_recurso/24_25_recurso_ex2.asm
+++ ex_spreadsheet/24_25_recurso/24_25_recurso_ex2.asm
@@ -7,7 +7,15 @@
 
 dseg    segment para public 'data'
     ; --- DEFINICAO DE VARIAVEIS ---
-    
+    notas db 10,12,8,15,16,20,19,3,13,14,0,11,10,17,7,'$'
+
+    cont_ins db 0
+    cont_suf db 0
+    cont_mb  db 0
+    cont_exc db 0
+    linha    db 5
+
+    letras_categorias db 'I', 'S', 'M', 'E'
 
 cseg    segment para public 'code'
     assume  cs:cseg, ds:dseg
@@ -28,5 +36,92 @@ fim:
 main    endp
 
 cseg    ends
-end     main~
+end     main
+
+
+; ###################################################################################
+; i. criacao do procedimento CLassificarNotas
+
+ClassificarNotas proc ; apenas iterar o array, e aumentar os contadores
+    ; adicionar push
+
+    xor si, si
+
+obter_num:
+    mov al, notas[si]
+    cmp al, '$'
+    je fim_reg ; verificar fim
+
+    cmp al, 9
+    jbe add_ins
+
+    cmp al, 13
+    jbe add_suf
+
+    cmp al, 18
+    jbe add_mb
+
+    inc cont_exc
+    jmp prox_nota
+
+add_ins:
+    inc cont_ins
+    jmp prox_nota
+
+add_suf:
+    inc cont_suf
+    jmp prox_nota
+
+add_mb:
+    inc cont_mb
+    jmp prox_nota
+
+prox_nota:
+    inc si
+    jmp obter_num
+
+fim_reg:
+    ; adicionar pop
+    ret
+ClassificarNotas endp
+
+
+
+
+
+; ###################################################################################
+; implementacao do procedimento EscreveLetra
+
+EscreveLetra proc
+    ; adicionar push
+    
+    ; si tem o caracter atraves do vetor das 4 opcoes
+    ; bl tenha a linha (indexado em 1, e nao 0) onde devemos escrever o caracter
+
+    ; 1. precisamos do offset para escrever a letra
+    ; offset = linha * 160
+    mov al, bl
+    dec al
+    mov ah, 160
+    mul ah
+    mov di, ax
+
+    ; 2. obter a letra que vamos escrever
+    mov al, letras_categorias[si]
+    
+    mov ah, 01110000b ; definir a cor do texto e do fundo, com blink = 0
+
+    ; 3. escrita na memoria de video da letra
+    mov es:[di], ax
+    
+    ; adicionar pop
+    ret
+EscreveLetra endp
+
+
+
+
+
+; ###################################################################################
+; implementacao do procedimento DesenhaBarraSimples
 
